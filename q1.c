50a755b4936ff981d57f6a128116bda5d9332b4d
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-09-13 17:46:28 +0430

    question 1 answer added

---
+++ q1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define ll long long
+
+
+long findLen(int num, int base)
+{
+    long len = 0;
+
+    while (powl(base, len) < num)
+        len++;
+    
+    return len;
+}
+
+
+void print(ll input)
+{
+    if (input < 10)
+    {
+        printf("%lld", input);
+        return;
+    }
+    
+    input = input%10;
+    printf("%c", (char)(65+input));
+}
+
+
+int main()
+{
+    ll num, base;
+    scanf("%lld %lld", &num, &base);
+
+
+    ll len = findLen(num, base);
+    ll output[len];
+    
+
+    long currIndex = 0;
+    while (num >= base)
+    {
+        output[currIndex] = num%base;
+        num = num/base;
+        currIndex++;
+    }
+    output[currIndex] = num;
+
+
+    for (int i = len-1; i >= 0; i--)
+        print(output[i]);
+
+
+    printf("\n");
+    return 0;
+}
